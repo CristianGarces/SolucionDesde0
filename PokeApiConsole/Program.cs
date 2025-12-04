cd8317e105a3dfa70f557d229a4bdfa434ac750a
CristianGarces <cristiangarces9205@gmail.com>
2025-12-04 18:07:46 +0100

    Peticion Http api externa (pokemon)

---
+++ PokeApiConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿// Poke api Http example
+using System.Text.Json;
+
+Uri uri = new Uri("https://pokeapi.co/api/v2/");
+
+HttpClient client = new HttpClient();
+
+var result = await client.GetAsync(uri + "pokemon/charmander");
+
+var content = await result.Content.ReadAsStringAsync();
+
+//Console.WriteLine(content);
+
+Pokemon pokemon = JsonSerializer.Deserialize<Pokemon>(content)!;    
+
+Console.WriteLine($"Pokemon name: {pokemon.name}");
+
