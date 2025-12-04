4ecf807c12442e1fbab81802bb97c5658bbc4b73
CristianGarces <cristiangarces9205@gmail.com>
2025-12-04 20:02:27 +0100

    - JWT in Orders

--- SolucionDesde0.Api.Orders/Program.cs
+++ SolucionDesde0.Api.Orders/Program.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using SolucionDesde0.API.Orders.Data;
 using SolucionDesde0.ServiceDefaults;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +16,25 @@ builder.AddNpgsqlDbContext<OrdersDbContext>("SolucionDesde0OrdersDb");
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// JWT Authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = builder.Configuration.GetSection("JWT:Issuer").Value!,
+                ValidAudience = builder.Configuration.GetSection("JWT:Audience").Value!,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetSection("JWT:SecretKey").Value!))
+            };
+        });
 
 var app = builder.Build();
 
@@ -27,6 +49,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
